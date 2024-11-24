57643723b8f84bf25314de2526fb1916ff1c1c60
sachuIsCool <102426225+sachuIsCool@users.noreply.github.com>
2024-11-24 19:23:10 +0000

    conversion of datatype

--- 03_conversionoperation.js
+++ 03_conversionoperation.js
@@ -17,3 +17,31 @@ console.log(booleanIsLoggedIn);
 //0 => false
 //"" => false
 //"sachin" => true
+//******************* operation***********
+
+let value =3
+let negValue =-value
+console.log(negValue);
+console.log(2-2);
+console.log(2+2);
+console.log(2*2);
+console.log(2**2);
+
+
+let str2="hello"
+let str1= "sachin"
+console.log(str2+str1);
+console.log("1"+2);
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log((2+2)*5%3);
+console.log(+true);
+console.log(+"");
+let um1,num2,num3
+num1=num2=num3=2+2
+let gamecounter=100
+gamecounter--;
+console.log(gamecounter);
+
+
